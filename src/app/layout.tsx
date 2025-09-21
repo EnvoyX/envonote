de16f82ffabfc4a4c4e326a3b33419e030dfb909
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-09-21 21:31:29 +0700

    feat: add sidebar (just UI)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,12 +38,15 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="flex min-h-screen w-full flex-col">
-            <Header />
-            <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-              {children}
-            </main>
-          </div>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
           <Toaster />
         </ThemeProvider>
       </body>
