93151f9a450f0fdf971172dfe79c42033e75a2db
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-09-22 21:41:41 +0700

    feat: NewNoteButton component done

--- src/action/notes.ts
+++ src/action/notes.ts
@@ -4,6 +4,25 @@ import { getUser } from "@/app/auth/server";
 import { prisma } from "@/db/prisma";
 import { handleError } from "@/lib/utils";
 
+export async function createNoteAction(noteId: string) {
+  try {
+    const user = await getUser();
+    if (!user) throw new Error("You must be logged in to update a note.");
+
+    await prisma.note.create({
+      data: {
+        id: noteId,
+        text: "",
+        authorId: user.id,
+      },
+    });
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
 export async function updateNoteAction(noteId: string, text: string) {
   try {
     const user = await getUser();
--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,13 +1,46 @@
 "use client";
 import { User } from "@supabase/supabase-js";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { toast } from "sonner";
+import { createNoteAction } from "@/action/notes";
 
 type Props = {
   user: User | null;
 };
 
 function NewNoteButton({ user }: Props) {
-  console.log(user?.email);
-  return <div>NewNoteButton</div>;
-}
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  async function handleClickNewNoteButton() {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setIsLoading(true);
 
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+
+      router.push(`/?noteId=${uuid}`);
+      toast.success("New note created!", {
+        description: "Your new note has been created.",
+      });
+
+      setIsLoading(false);
+    }
+  }
+  return (
+    <Button
+      onClick={handleClickNewNoteButton}
+      variant={"secondary"}
+      className="w-24"
+      disabled={isLoading}
+    >
+      {isLoading ? <Loader2 className="animate-spin" /> : "New Note"}
+    </Button>
+  );
+}
 export default NewNoteButton;
